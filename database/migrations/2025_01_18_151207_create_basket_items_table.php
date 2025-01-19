0df3a8e93943b5b585ab72aaf88a3d148eb58b4c
jelle.monen <jelle.monen@true.nl>
2025-01-19 18:17:12 +0100

    chore: add migrations

---
+++ database/migrations/2025_01_18_151207_create_basket_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('basket_items', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('basket_id');
+            $table->uuid('product_id');
+            $table->foreign('basket_id')->references('id')->on('baskets')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('quantity')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('basket_items');
+    }
+};
